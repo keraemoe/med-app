fd6eda30ed684f5bf59e7083842e584dad46f492
keraemoe <weekera@gmail.com>
2022-11-21 00:14:33 +0600

    remove: remove Hero

--- src/components/HomeComponents/sectionHero/Hero.jsx
+++ src/components/HomeComponents/sectionHero/Hero.jsx
@@ -7,43 +7,7 @@ const Hero = () => {
     return (
         <section id={s.hero}>
             <div className={s.container}>
-                <div>
-                    <h1>
-                        <span>
-                            We care <br />
-                        </span>
-                        about your health
-                    </h1>
-                    <p>
-                        Good health is the state of mental, physical and social well being <br /> and it does not just mean absence of diseases.
-                    </p>
-                    <div className={s.container_main_btn}>
-                        <h1>Find a Doctor</h1>
-                        <div>
-                            <input type="text" />
-                            <input type="text" />
-                        </div>
-                    </div>
-                </div>
-                <div>
-                    <div className={s.container_btns}>
-                        <button className={s.search}>
-                            <img src={SearchGroup} alt="search" />
-                            <a>
-                                Well Qualified doctors
-                            </a>
-                        </button>
-                        <button className={s.book}>
-                            <img src={Group} alt="group" />
-                            <a>
-                                Book an appointment.
-                            </a>
-                        </button>
-                    </div>
-                </div>
-            </div>
-            <div>
-
+                
             </div>
         </section>
     );
