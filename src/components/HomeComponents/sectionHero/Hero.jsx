5405617bd19c3a48a702bc501ae5b2a1a9b1723b
keraemoe <weekera@gmail.com>
2022-11-21 19:58:20 +0600

    add: adaptive and remake header and hero

--- src/components/HomeComponents/sectionHero/Hero.jsx
+++ src/components/HomeComponents/sectionHero/Hero.jsx
@@ -7,7 +7,25 @@ const Hero = () => {
     return (
         <section id={s.hero}>
             <div className={s.container}>
-                
+                <div className={s.section_hero}>
+                    <div className={s.hero_left}>
+                        <h1>We care <br /> <span>about your health</span></h1>
+                        <p>Good health is the state of mental, physical and social well being <br /> and it does not just mean absence of diseases.</p>
+                        <span> Become member of our hospital community?<a href="#">Sign Up</a>
+                        </span>
+                        <div className={s.container_btn}>
+                            <a>Find a doctor</a>
+                            <div>
+                                <input type="text" value="Name of Doctor" />
+                                <input type="text" value="Speciality" />
+                                <button>
+                                    Search
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={s.hero_right}></div>
+                </div>
             </div>
         </section>
     );
