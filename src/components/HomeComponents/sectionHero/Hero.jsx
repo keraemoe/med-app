6db75a56f23e40ab8e2826f896436018131dab22
keraemoe <weekera@gmail.com>
2022-12-02 20:20:03 +0600

    input

--- src/components/HomeComponents/sectionHero/Hero.jsx
+++ src/components/HomeComponents/sectionHero/Hero.jsx
@@ -5,32 +5,54 @@ import SearchGroup from '../../../assets/SearchGroup.png'
 // import ParticlesBackground from '../../Layouts/ParticlesBackground/ParticlesBackground';
 
 const Hero = () => {
-    return (
-        <section id={s.hero}>
-            {/* <ParticlesBackground/> */}
-            <div className={s.container}>
-                <div className={s.section_hero}>
-                    <div className={s.hero_left}>
-                        <h1>We care <br /> <span>about your health</span></h1>
-                        <p>Good health is the state of mental, physical and social well being <br /> and it does not just mean absence of diseases.</p>
-                        <span> Become member of our hospital community?<a href="#">Sign Up</a>
-                        </span>
-                        <div className={s.container_btn}>
-                            <a>Find a doctor</a>
-                            <div>
-                                <input type="text" value="Name of Doctor" />
-                                <input type="text" value="Speciality" />
-                                <button>
-                                    Search
-                                </button>
+    const [message, setMessage] = useState("Name of Doctor");
+    const [message2, setMessage2] = useState("Speciality");
+
+    const handleChange = (event) => {
+        setMessage(event.target.value);
+    };
+
+    const handleChange2 = (e) => {
+        setMessage2(e.target.value);
+    };
+
+    const Hero = () => {
+        return (
+            <section id={s.hero}>
+                {/* <ParticlesBackground/> */}
+                <div className={s.container}>
+                    <div className={s.section_hero}>
+                        <div className={s.hero_left}>
+                            <h1>We care <br /> <span>about your health</span></h1>
+                            <p>Good health is the state of mental, physical and social well being <br /> and it does not just mean absence of diseases.</p>
+                            <span> Become member of our hospital community?<a href="#">Sign Up</a>
+                            </span>
+                            <div className={s.container_btn}>
+                                <a>Find a doctor</a>
+                                <div>
+                                    <input
+                                        type="text"
+                                        defaultValue="Name of Doctor"
+                                        onChange={handleChange}
+                                        value={message}
+                                    />
+                                    <input
+                                        type="text"
+                                        defaultValue="Speciality"
+                                        onChange={handleChange}
+                                        value={message2}
+                                    />
+                                    <button>
+                                        Search
+                                    </button>
+                                </div>
                             </div>
                         </div>
+                        <div className={s.hero_right}></div>
                     </div>
-                    <div className={s.hero_right}></div>
                 </div>
-            </div>
-        </section>
-    );
-};
+            </section>
+        );
+    };
 
-export default Hero;
+    export default Hero;
