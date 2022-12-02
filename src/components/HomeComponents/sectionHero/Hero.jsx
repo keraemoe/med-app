ce781362ebe6a847f070b42dbde7f12683f9b578
keraemoe <weekera@gmail.com>
2022-12-02 20:21:32 +0600

    add: input edit

--- src/components/HomeComponents/sectionHero/Hero.jsx
+++ src/components/HomeComponents/sectionHero/Hero.jsx
@@ -1,58 +1,60 @@
-﻿import React from 'react';
-import s from './Hero.module.css'
-import Group from '../../../assets/Group.png'
-import SearchGroup from '../../../assets/SearchGroup.png'
+﻿import React from "react";
+import s from "./Hero.module.css";
+import { useState } from "react";
 // import ParticlesBackground from '../../Layouts/ParticlesBackground/ParticlesBackground';
 
 const Hero = () => {
-    const [message, setMessage] = useState("Name of Doctor");
-    const [message2, setMessage2] = useState("Speciality");
+  const [message, setMessage] = useState("Name of Doctor");
+  const [message2, setMessage2] = useState("Speciality");
 
-    const handleChange = (event) => {
-        setMessage(event.target.value);
-    };
+  const handleChange = (event) => {
+    setMessage(event.target.value);
+  };
 
-    const handleChange2 = (e) => {
-        setMessage2(e.target.value);
-    };
+  const handleChange2 = (e) => {
+    setMessage2(e.target.value);
+  };
 
-    const Hero = () => {
-        return (
-            <section id={s.hero}>
-                {/* <ParticlesBackground/> */}
-                <div className={s.container}>
-                    <div className={s.section_hero}>
-                        <div className={s.hero_left}>
-                            <h1>We care <br /> <span>about your health</span></h1>
-                            <p>Good health is the state of mental, physical and social well being <br /> and it does not just mean absence of diseases.</p>
-                            <span> Become member of our hospital community?<a href="#">Sign Up</a>
-                            </span>
-                            <div className={s.container_btn}>
-                                <a>Find a doctor</a>
-                                <div>
-                                    <input
-                                        type="text"
-                                        defaultValue="Name of Doctor"
-                                        onChange={handleChange}
-                                        value={message}
-                                    />
-                                    <input
-                                        type="text"
-                                        defaultValue="Speciality"
-                                        onChange={handleChange}
-                                        value={message2}
-                                    />
-                                    <button>
-                                        Search
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                        <div className={s.hero_right}></div>
-                    </div>
-                </div>
-            </section>
-        );
-    };
+  return (
+    <section id={s.hero}>
+      {/* <ParticlesBackground/> */}
+      <div className={s.container}>
+        <div className={s.section_hero}>
+          <div className={s.hero_left}>
+            <h1>
+              We care <br /> <span>about your health</span>
+            </h1>
+            <p>
+              Good health is the state of mental, physical and social well being{" "}
+              <br /> and it does not just mean absence of diseases.
+            </p>
+            <span>
+              Become member of our hospital community?<a href="#">Sign Up</a>
+            </span>
+            <div className={s.container_btn}>
+              <a>Find a doctor</a>
+              <div>
+                <input
+                  type="text"
+                  defaultValue="Name of Doctor"
+                  onChange={handleChange}
+                  value={message}
+                />
+                <input
+                  type="text"
+                  defaultValue="Speciality"
+                  onChange={handleChange}
+                  value={message2}
+                />
+                <button>Search</button>
+              </div>
+            </div>
+          </div>
+          <div className={s.hero_right}></div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-    export default Hero;
+export default Hero;
